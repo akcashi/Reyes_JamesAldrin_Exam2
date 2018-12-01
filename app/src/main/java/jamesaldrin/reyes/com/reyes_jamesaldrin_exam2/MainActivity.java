6452e53c6dcd2a58f9c360dc59495825692efdc2
James <jamesaldrin.reyes.iics@ust.edu.ph>
2018-12-01 14:20:23 +0800

    Initial commit

---
+++ app/src/main/java/jamesaldrin/reyes/com/reyes_jamesaldrin_exam2/MainActivity.java
@@ -0,0 +1,107 @@
+package jamesaldrin.reyes.com.reyes_jamesaldrin_exam2;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity {
+    EditText Fname, Lname, Exam1, Exam2, Average;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Fname = findViewById(R.id.Fname);
+        Lname = findViewById(R.id.Lname);
+        Exam1 = findViewById(R.id.Exam1);
+        Exam2 = findViewById(R.id.Exam2);
+        Average = findViewById(R.id.Average);
+    }
+
+
+    public void saveData(View v)
+    {
+        String FName = Fname.getText().toString();
+        String LName = Lname.getText().toString();
+        String EXam1 = Exam1.getText().toString();
+        String EXam2 = Exam2.getText().toString();
+        int Score1 = Integer.parseInt(EXam1);
+        int Score2 = Integer.parseInt(EXam2);
+        float average = (Score1 + Score2)/2;
+
+        String averagedisplay = Float.toString(average);
+
+        SharedPreferences sp = getSharedPreferences("Data1", MODE_PRIVATE);
+        SharedPreferences.Editor writer = sp.edit();
+        writer.putString("first", FName);
+        writer.putString("last", LName);
+        writer.putString ("average", averagedisplay);
+        writer.commit();
+        Toast.makeText(this, "Saved.", Toast.LENGTH_SHORT).show();
+    }
+
+    public void saveExternal(View v)
+    {
+        File folder = getExternalFilesDir("TempFolder");
+        File file = new File(folder, "Data1.txt");
+        String FName = Fname.getText().toString();
+        String LName = Lname.getText().toString();
+        String EXam1 = Exam1.getText().toString();
+        String EXam2 = Exam2.getText().toString();
+        int Score1 = Integer.parseInt(EXam1);
+        int Score2 = Integer.parseInt(EXam2);
+        float average = (Score1 + Score2)/2;
+
+        String averagedisplay = Float.toString(average);
+        FileOutputStream fos = null;
+        try {
+            fos = openFileOutput("Data1.txt", MODE_PRIVATE);
+            fos.write(FName.getBytes());
+            fos.write(LName.getBytes());
+            fos.write(averagedisplay.getBytes());
+        } catch (Exception e) {
+            Log.d("Error!", "Error!");
+        }
+        finally {
+            try {
+                fos.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        Toast.makeText(this, "Success! Data placed at " + getExternalFilesDir("Data1.txt"), Toast.LENGTH_SHORT).show();
+        loadExternal();
+
+    }
+
+    private void loadExternal()
+    {
+        File folder = getExternalFilesDir("TempFolder");
+        File file = new File(folder, "Data1.txt");
+        try {
+            FileInputStream fis = openFileInput("Data1.txt");
+            int read = 0;
+            StringBuffer buffer = new StringBuffer();
+
+            while((read = fis.read()) != -1)
+            {
+                buffer.append((char)read);
+            }
+            String averages = buffer + "!";
+            Average.setText(averages);
+
+        } catch (Exception e) {
+            Log.d("Error", "ERROR READING DATA!");
+        }
+
+    }
+}
